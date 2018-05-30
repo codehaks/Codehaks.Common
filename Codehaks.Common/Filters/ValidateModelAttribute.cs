841989d6a60bf938b3e296944c823bab409151ef
Hakim Ghods <ghods.hakim@hotmail.com>
2018-05-30 17:23:36 +0430

    Add project files.

---
+++ Codehaks.Common/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Codehaks.Common.Filters
+{
+    public class ValidateModelAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var ctrl = (Controller)context.Controller;
+
+                var view = new ViewResult
+                {
+                    ViewName = context.RouteData.Values["Action"].ToString(),
+                    ViewData = ctrl.ViewData
+                };
+
+                context.Result = view;
+            }
+        }
+    }
+}
